f2968585af0f59323d80f6f417cd389599b7177d
thebigtoad <thebigtoad@users.noreply.github.com>
2017-10-27 22:12:15 +0100

    Configure default fzf ctrl-t command

--- .fzf.zsh
+++ .fzf.zsh
@@ -14,3 +14,8 @@ source "/home/dev/.vim/bundle/fzf/shell/key-bindings.zsh"
 
 # Use ag to provide the source, including hidden files, ignoring .git but respecting .gitgnore
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+#ag doesn't seem able to list dirs at the moment so stuck with find
+#export FZF_ALT_C_COMMAND=''
