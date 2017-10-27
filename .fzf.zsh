b37950e5c5057a08ad76c834bed71583c355f33e
thebigtoad <thebigtoad@users.noreply.github.com>
2017-10-27 21:59:38 +0100

    Configure fzf's default command

--- .fzf.zsh
+++ .fzf.zsh
@@ -12,3 +12,5 @@ fi
 # ------------
 source "/home/dev/.vim/bundle/fzf/shell/key-bindings.zsh"
 
+# Use ag to provide the source, including hidden files, ignoring .git but respecting .gitgnore
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
